f8d3a9ceda701f6c7e97bf7542402a40ec7e3235
Mikael Westermann <madsherlock@users.noreply.github.com>
2023-12-01 10:50:17 +0100

    Update __init__.py

--- detectron2/export/__init__.py
+++ detectron2/export/__init__.py
@@ -5,17 +5,6 @@ import warnings
 from .flatten import TracingAdapter
 from .torchscript import dump_torchscript_IR, scripting_with_instances
 
-try:
-    from caffe2.proto import caffe2_pb2 as _tmp
-    from caffe2.python import core
-
-    # caffe2 is optional
-except ImportError:
-    pass
-else:
-    from .api import *
-
-
 # TODO: Update ONNX Opset version and run tests when a newer PyTorch is supported
 STABLE_ONNX_OPSET_VERSION = 16
 
